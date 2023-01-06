a4afae7a76b4ba7322c0f3501bd21633f3f7fb82
NikSun <candysfm@gmail.com>
2023-01-06 22:08:42 +0800

    fix

--- r.sh
+++ r.sh
@@ -2,14 +2,15 @@
 
 cp /lib/ld-musl-x86_64.so.1 lib/
 cp /usr/bin/qemu-mipsel usr/bin/qemu-mipsel
+cp /etc/resolv.conf etc/
 
 mount --bind /dev dev
 mount --bind /proc proc
 mount --bind /sys sys
 
 if [ -d "/share" ]; then
-  mkdir share
-  mount --bind /share share
+  mkdir /root/share
+  mount -o move /share /root/share
 fi
 
 chroot . qemu-mipsel -E LD_LIBRARY_PATH=/lib:/opt/lib,PATH=/opt/sbin:/opt/bin:/usr/sbin:/usr/bin:/sbin:/bin "$@"
